b86833a796d9895de2740a451cf7cee6a98d4f64
Donna <donnajun1214@gmail.com>
2023-08-10 17:07:15 +0900

    [add] loader, buttons, headers

---
+++ lib/src/components/header_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../core/lala_style.dart';
+
+class HeaderText extends StatelessWidget {
+  const HeaderText(this.label, {super.key});
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      label,
+      style: LalaCommonStyle.header,
+    );
+  }
+}
+
+class DialogHeaderText extends StatelessWidget {
+  const DialogHeaderText(this.label, {super.key});
+  final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      label,
+      style: LalaCommonStyle.dialogHeader,
+    );
+  }
+}
